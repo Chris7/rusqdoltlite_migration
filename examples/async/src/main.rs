d797629e2854ec80e3601b8e6983946b97f73da1
Chris Mitchell <chris.mit7@gmail.com>
2026-06-26 22:05:07 -0400

    Use doltlite

--- examples/async/src/main.rs
+++ examples/async/src/main.rs
@@ -14,15 +14,14 @@
 // limitations under the License.
 
 use anyhow::Result;
-use rusqlite::params;
+use rusqlite::{params, Connection};
 use rusqlite_migration::{Migrations, M};
-use tokio_rusqlite_new::Connection;
+use tokio::task;
 
-/// The general idea with this example is to use [`Connection::call`][call] and
-/// [`Connection::call_unwrap`][call_unwrap] to run the migration in a sync context.
+/// The general idea with this example is to use [`spawn_blocking`][spawn_blocking]
+/// to run migrations in a sync context without blocking Tokio worker threads.
 ///
-/// [call]: https://docs.rs/tokio-rusqlite/0.6.0/tokio_rusqlite/struct.Connection.html#method.call
-/// [call_unwrap]: https://docs.rs/tokio-rusqlite/0.6.0/tokio_rusqlite/struct.Connection.html#method.call_unwrap
+/// [spawn_blocking]: https://docs.rs/tokio/latest/tokio/task/fn.spawn_blocking.html
 
 // Test that migrations are working
 #[cfg(test)]
@@ -57,77 +56,46 @@ const MIGRATION_ARRAY: &[M] = &[
 const MIGRATIONS: Migrations = Migrations::from_slice(MIGRATION_ARRAY);
 
 pub async fn init_db() -> Result<Connection> {
-    let async_conn = Connection::open("./my_db.db3").await?;
-
-    // Update the database schema, atomically
-    // Using `call_unwrap` is appropriate because we have just opened the connection before and
-    // this function still returns any error returned inside of it. In other words, according to
-    // the [documentation][docs], it only panics if there is an issue with the connection, not if
-    // an error is returned by the closure.
-    //
-    // [docs]: https://docs.rs/tokio-rusqlite/0.6.0/tokio_rusqlite/struct.Connection.html#method.call_unwrap
-    async_conn
-        .call_unwrap(|conn| MIGRATIONS.to_latest(conn))
-        .await?;
-
-    Ok(async_conn)
+    task::spawn_blocking(|| {
+        let mut conn = Connection::open("./my_db.db3")?;
+        // Update the database schema, atomically.
+        MIGRATIONS.to_latest(&mut conn)?;
+        Ok(conn)
+    })
+    .await?
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("trace")).init();
 
-    let async_conn = init_db().await.unwrap();
+    let mut conn = init_db().await?;
 
-    // Apply some PRAGMA. These are often better applied outside of migrations, as some needs to be
-    // executed for each connection (like `foreign_keys`) or to be executed outside transactions
-    // (`journal_mode` is a noop in a transaction).
-    async_conn
-        .call::<_, _, rusqlite::Error>(|conn| Ok(conn.pragma_update(None, "journal_mode", "WAL")))
-        .await
-        .unwrap()
-        .unwrap();
-    async_conn
-        .call::<_, _, rusqlite::Error>(|conn| Ok(conn.pragma_update(None, "foreign_keys", "ON")))
-        .await
-        .unwrap()
-        .unwrap();
+    task::spawn_blocking(move || -> Result<()> {
+        // Apply some PRAGMA. These are often better applied outside of migrations, as some needs to be
+        // executed for each connection (like `foreign_keys`) or to be executed outside transactions
+        // (`journal_mode` is a noop in a transaction).
+        conn.pragma_update(None, "journal_mode", "WAL")?;
+        conn.pragma_update(None, "foreign_keys", "ON")?;
 
-    // Use the db 🥳
-    async_conn
-        .call::<_, _, rusqlite::Error>(|conn| {
-            Ok(conn.execute(
-                "INSERT INTO friend (name, birthday) VALUES (?1, ?2)",
-                params!["John", "1970-01-01"],
-            ))
-        })
-        .await
-        .unwrap()
-        .unwrap();
+        // Use the db 🥳
+        conn.execute(
+            "INSERT INTO friend (name, birthday) VALUES (?1, ?2)",
+            params!["John", "1970-01-01"],
+        )?;
 
-    async_conn
-        .call::<_, _, rusqlite::Error>(|conn| {
-            Ok(conn.execute("INSERT INTO animal (name) VALUES (?1)", params!["dog"]))
-        })
-        .await
-        .unwrap()
-        .unwrap();
+        conn.execute("INSERT INTO animal (name) VALUES (?1)", params!["dog"])?;
 
-    // We can revert to the last migration
-    // Let’s also demonstrate using `Connection::call` instead of `Connection::call_unwrap`.
-    // Notice how in effect, we have handle two `Result`s, one for the errors happening when we try
-    // to use the connection and the other one when applying the migration proper.
-    async_conn
-        .call::<_, _, rusqlite::Error>(|conn| Ok(MIGRATIONS.to_version(conn, 2)))
-        .await??;
+        // We can revert to the last migration.
+        MIGRATIONS.to_version(&mut conn, 2)?;
 
-    // The table was removed
-    async_conn
-        .call::<_, _, rusqlite::Error>(|conn| {
-            Ok(conn.execute("INSERT INTO animal (name) VALUES (?1)", params!["cat"]))
-        })
-        .await
-        .unwrap_err();
+        // The table was removed.
+        conn.execute("INSERT INTO animal (name) VALUES (?1)", params!["cat"])
+            .unwrap_err();
+
+        Ok(())
+    })
+    .await??;
 
     Ok(())
 }
